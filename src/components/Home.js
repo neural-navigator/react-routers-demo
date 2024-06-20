814e11ec233b65916b0a72c3e01e3a88580e9331
neural-navigator <iamsatyapati@gmail.com>
2024-06-20 08:28:38 +0530

    back routing added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div>
       Homepage
+      <button onClick={() => navigate('order-summary', {replace: true})}>Place Order!</button>
     </div>
   )
 }
--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function OrderSummary() {
+    const navigate = useNavigate();
+
   return (
     <div>
       Order Confirmed!
+       {/* -1 represents one step back in navigation */}
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   )
 }
