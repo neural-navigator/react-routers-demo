6521bd3022a9613d2b43d46d6ed0a15f73db8383
neural-navigator <iamsatyapati@gmail.com>
2024-06-21 07:34:31 +0530

    dynamic routing added

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
 
 const UserDetails = () => {
+    const params = useParams()
+    const userId = params.userId
   return (
     <div>
-      Details of the user
+      Details of the user {userId}
     </div>
   )
 }
