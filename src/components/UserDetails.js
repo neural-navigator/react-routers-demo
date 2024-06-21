4c73decefe6060a1219d28e9d963e0112b960ccb
neural-navigator <iamsatyapati@gmail.com>
2024-06-21 07:53:59 +0530

    url param routing added

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,7 @@
 import { useParams } from "react-router-dom"
 
 const UserDetails = () => {
-    const params = useParams()
-    const userId = params.userId
+    const {userId} = useParams()
   return (
     <div>
       Details of the user {userId}
